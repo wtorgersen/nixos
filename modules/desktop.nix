0555a670457fa86b0ce718f3e7513bd38a5ee6b2
William Torgersen <william_torgersen@hotmail.com>
2026-08-08 13:56:53 +0200

    Removed ibus as this is not needed with Cosmic

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -10,16 +10,6 @@
 { config, pkgs, ... }:
 
 {
-  # ------------------------------------------------------------
-  # Input methods
-  # ------------------------------------------------------------
-  #
-  i18n.inputMethod = {
-    enable = true;
-    type = "ibus";
-    ibus.engines = with pkgs.ibus-engines; [ /* any engine you want, for example */ anthy ];
-  };
-
   services.libinput.enable = true;
   
   # -----------------------------------------------------------
