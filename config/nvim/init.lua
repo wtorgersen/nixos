356a1badff978d88ea4ce3c48e5adc29ff4908ba
William Torgersen <william_torgersen@hotmail.com>
2026-08-12 18:05:56 +0200

    Converted to new .lua config

---
+++ config/nvim/init.lua
@@ -0,0 +1,31 @@
+-- NeoVim config
+
+-- LazyVim plugin manager
+require("config.lazy")
+
+-- Appearance
+vim.opt.number = true
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Indentation
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+-- Leader key
+vim.g.mapleader = " "
+
+-- Keybinds
+--
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- Transparent background
+vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
